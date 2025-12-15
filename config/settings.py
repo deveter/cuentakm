f02b95aa58669044db5df58277894f771b8f329a
PPF <pedropinarfilpo@gmail.com>
2025-12-15 16:58:23 +0100

    Cambios en la base de datos

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # ---------------------------------------------------------
 # Cargar archivo .env correctamente
@@ -82,10 +83,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=not DEBUG,
+    )
 }
 
 
