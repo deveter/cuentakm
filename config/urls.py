dfa37199b2c6c44ba68b8e89ab4516aba46ef0a0
PPF <pedropinarfilpo@gmail.com>
2026-01-26 12:07:58 +0100

    Se ven los media en produccion

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse, HttpResponseNotFound
 from django.conf import settings
+from django.conf.urls.static import static
 from pathlib import Path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 def spa_index(request):
+    """
+    Sirve el index.html del frontend (Vue SPA)
+    - En DEBUG: desde backend/static/dist/index.html
+    - En producción: desde STATIC_ROOT/dist/index.html
+    """
     if settings.DEBUG:
         index_path = Path(settings.BASE_DIR) / "static" / "dist" / "index.html"
     else:
@@ -16,10 +22,18 @@ def spa_index(request):
 
     return HttpResponse(index_path.read_text(encoding="utf-8"))
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("lecturas.urls")),
     path("", spa_index),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+# -----------------------------
+# SERVIR MEDIA SOLO EN DEBUG
+# -----------------------------
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
